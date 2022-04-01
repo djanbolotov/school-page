35a09bb21f4a624167a751818ae9af8d50b73250
Askabek <djanbolotov03@gmail.com>
2022-04-01 21:58:12 +0600

    Updates

---
+++ precache-manifest.827aed80efb369ae913976ab2c325c8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9de70be8a2fe66fb1b6",
+    "url": "/school-page/static/css/main.0bc11912.chunk.css"
+  },
+  {
+    "revision": "e9de70be8a2fe66fb1b6",
+    "url": "/school-page/static/js/main.e9de70be.chunk.js"
+  },
+  {
+    "revision": "e2695dfe374e4a343334",
+    "url": "/school-page/static/js/1.e2695dfe.chunk.js"
+  },
+  {
+    "revision": "8b45029cca8ce673fc74",
+    "url": "/school-page/static/js/runtime~main.8b45029c.js"
+  },
+  {
+    "revision": "73e176f4970c10e949229f46a576b37d",
+    "url": "/school-page/static/media/UserImage.73e176f4.png"
+  },
+  {
+    "revision": "cf19bf2e658e92c9c1aec6c76b837f72",
+    "url": "/school-page/static/media/Logo.cf19bf2e.png"
+  },
+  {
+    "revision": "13654c6847df1ee27fcca5e00ce93230",
+    "url": "/school-page/static/media/Login.13654c68.png"
+  },
+  {
+    "revision": "8cc2196eaab29e9cd1e0408eca2c7ce6",
+    "url": "/school-page/index.html"
+  }
+];
